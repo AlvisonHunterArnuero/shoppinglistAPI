f9d4df9883135fa197bf4928c2445ce92028e5ea
Alvison Hunter Arnuero <alvison@gmail.com>
2020-12-14 07:48:12 -0600

    backing up files

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
-//The import of the express module goes first
-const express = require("express");
-//Now I will declare an instance of express for my app
+/* eslint-disable no-console */
+// The import of the express module goes first
+const express = require('express');
+
+// Routes to be used on the app
+const routes = require('./routes');
+
+// Now I will declare an instance of express for my app
 const app = express();
+
 // declare the port that I will use on this app
 const port = process.env.PORT || 8080;
+
 // I need to declare 2 methods for handling http encoded url and json objects
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.set('views', './views');
 
-// Here I will declare the views that I will use on this app
-const home = require("./views/home");
-const about = require("./views/about");
-const contactus = require("./views/contact");
-
-// Time to use those views with their respective route
-app.use("/", home);
-app.use("/about", about);
-app.use("/contact", contactus);
-
-app.locals.title = "You are currently on: ";
+app.locals.title = 'You are currently on: ';
 app.locals.menu =
   "<a href='/'>HOME</a> | <a href='/about'>ABOUT US</a> | <a href='/contact'>GET IN CONTACT</a>";
 
+routes(app);
 // We are going to se a listener for the server to trace changes
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Express server is listening at http://localhost:${port}`);
 });
 // last but not least, let us export the app
 module.exports = app;
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri =
+//   'mongodb+srv://ahuntera2020:<password>@cluster0.yqjb8.mongodb.net/<dbname>?retryWrites=true&w=majority';
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect((err) => {
+//   const collection = client.db('test').collection('devices');
+//   // perform actions on the collection object
+//   client.close();
+// });
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
-const express = require("express");
-const router = express.Router();
-router.get("/", (req, res) => {
-  res.send("<h1>Welcome to our shopping list API!</h1>");
-});
+const { home, about, contact } = require('../views/index.js');
 
-router.get("/about", (req, res) => {
-  res.send("<h3>Hey, Now you are in the about us page, pal!</h3>");
-});
-router.get("/contact", (req, res) => {
-  res.send("<h3>Drop us some lines here, fellow!</h3>");
-});
+const routes = (app) => {
+  app.get('/', home);
+  app.get('/about', about);
+  app.get('/contact', contact);
+};
+
+module.exports = routes;
+
